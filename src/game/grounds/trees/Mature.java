cc2634e1384dd5e1d28b3e6e44d653ca8a15bb37
apas0022 <apas0022@student.monash.edu>
2022-04-12 15:17:47 +1000

    Added age to Mature.java (increments by 1 each tick)

--- src/game/grounds/trees/Mature.java
+++ src/game/grounds/trees/Mature.java
@@ -8,13 +8,17 @@ import java.util.Random;
 public class Mature extends Ground {
 
     private final Random rand = new Random();
+    private int age;
 
     public Mature() {
         super('T');
+        age = 0;
     }
 
     @Override
     public void tick(Location location) {
+        age++;
+
         // 15% chance to summon a Koopa
         if (rand.nextInt(100) < 15) {
             //TODO: location.addActor(new Koopa());
