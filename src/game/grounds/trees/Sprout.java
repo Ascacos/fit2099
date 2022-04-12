f2628fa4ceec48082e16b51e762158b44067987f
apas0022 <apas0022@student.monash.edu>
2022-04-12 15:14:00 +1000

    Added overidden tick function with 10% chance to summon a Goomba

--- src/game/grounds/trees/Sprout.java
+++ src/game/grounds/trees/Sprout.java
@@ -1,10 +1,24 @@
 package game.grounds.trees;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.enemies.Goomba;
+
+import java.util.Random;
 
 public class Sprout extends Ground {
 
+    private final Random rand = new Random();
+
     public Sprout() {
         super('+');
+    }
+
+    @Override
+    public void tick(Location location) {
+        // 10% chance to summon a Goomba
+        if (rand.nextInt(100) < 10) {
+            location.addActor(new Goomba());
+        }
     }
 }
