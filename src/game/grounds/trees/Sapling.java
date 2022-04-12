bfad2b6b17f62c35a746a35a864e97f776cc06d0
apas0022 <apas0022@student.monash.edu>
2022-04-12 15:13:34 +1000

    Added overidden tick function with 10% chance to summon a Coin

--- src/game/grounds/trees/Sapling.java
+++ src/game/grounds/trees/Sapling.java
@@ -1,10 +1,23 @@
 package game.grounds.trees;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.Random;
 
 public class Sapling extends Ground {
 
+    private final Random rand = new Random();
+
     public Sapling() {
         super('t');
+    }
+
+    @Override
+    public void tick(Location location) {
+        // 10% chance to summon a Coin
+        if (rand.nextInt(100) < 10) {
+            //TODO: Summon a coin ($20)
+        }
     }
 }
