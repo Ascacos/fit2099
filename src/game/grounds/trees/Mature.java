1fe6d01be2921a1db4e2ab4ba961de23f743ba72
apas0022 <apas0022@student.monash.edu>
2022-04-12 15:12:52 +1000

    Added overidden tick function with 15% chance to summon a Koopa (Koopa not implemented yet)

--- src/game/grounds/trees/Mature.java
+++ src/game/grounds/trees/Mature.java
@@ -1,10 +1,23 @@
 package game.grounds.trees;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.Random;
 
 public class Mature extends Ground {
 
+    private final Random rand = new Random();
+
     public Mature() {
         super('T');
+    }
+
+    @Override
+    public void tick(Location location) {
+        // 15% chance to summon a Koopa
+        if (rand.nextInt(100) < 15) {
+            //TODO: location.addActor(new Koopa());
+        }
     }
 }
