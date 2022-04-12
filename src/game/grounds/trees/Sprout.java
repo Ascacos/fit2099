f59447ef5cd7e9e5e52b3ad65819be1583146b23
apas0022 <apas0022@student.monash.edu>
2022-04-12 15:16:54 +1000

    Age is now initialised at 0 in the constructor, not as an attribute

--- src/game/grounds/trees/Sprout.java
+++ src/game/grounds/trees/Sprout.java
@@ -9,10 +9,11 @@ import java.util.Random;
 public class Sprout extends Ground {
 
     private final Random rand = new Random();
-    private int age = 0;
+    private int age;
 
     public Sprout() {
         super('+');
+        age = 0;
     }
 
     @Override
