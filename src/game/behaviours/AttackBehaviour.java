95901c23b045cd1adf7631a9689f2a6cc5dd89b9
apas0022 <apas0022@student.monash.edu>
2022-04-26 13:00:32 +1000

    Implemented missing methods from AttackBehaviour.java, so that it works (fundamentally) while testing

--- src/game/behaviours/AttackBehaviour.java
+++ src/game/behaviours/AttackBehaviour.java
@@ -2,13 +2,27 @@ package game.behaviours;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.AttackAction;
 
 public class AttackBehaviour implements Behaviour{
+
+    private final Actor target;
+
+    public AttackBehaviour(Actor actor, Actor target) {
+        this.target = target;
+    }
 
     // TODO: develop and use it to attack the player automatically.
     @Override
     public Action getAction(Actor actor, GameMap map) {
+        for (Exit exit : map.locationOf(actor).getExits()) {
+            if (exit.getDestination().getActor() == target) {
+                System.out.println("attack action");
+                return new AttackAction(target, exit.getName());
+            }
+        }
         return null;
     }
 }
