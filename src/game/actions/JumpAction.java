3cc141ee05ab0136545cd02a0d7c1b88182f81c1
apas0022 <apas0022@student.monash.edu>
2022-04-27 20:34:23 +1000

    Added missing functionality to JumpAction.java

--- src/game/actions/JumpAction.java
+++ src/game/actions/JumpAction.java
@@ -4,27 +4,70 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
+
+import java.util.Random;
+
 
 public class JumpAction extends Action {
 
-    private Location destination;
-    private String direction;
+    private final Random rand = new Random();
+    private final Location destination;
+    private final String direction;
+    private final int successRate;
+    private final int failDamage;
 
-    public JumpAction(Location destination, String direction) {
+    /**
+     *
+     *
+     * @param destination The location to jump to
+     * @param direction The direction to jump in (cardinal diredction), relative to the Actor's location
+     * @param successRate The percent chance of the jump succeeding
+     * @param failDamage The dmaage the actor will take if the
+     */
+    public JumpAction(Location destination, String direction, int successRate, int failDamage) {
         this.destination = destination;
         this.direction = direction;
+        this.successRate = successRate;
+        this.failDamage = failDamage;
     }
 
+    /**
+     * Method to determine the outcome of a jump action, taking into account the success rate and
+     * fail damage (which varies depending on the ground type)
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return A description of what happened
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (destination.containsAnActor()) {
             return actor + " cant go there";
         } else {
-            map.moveActor(actor, destination);
+            //if the Actor has an active Super Mushroom, jump instantly
+            if (actor.hasCapability(Status.SUPER_MUSHROOM)) {
+                map.moveActor(actor, destination);
+                return actor + "jumped " + direction;
+            }
+
+            // evaluate % chance of successfully jumping
+            if (rand.nextInt(101) <= successRate) {
+                map.moveActor(actor, destination);
+                return actor + "jumped " + direction;
+            } else {
+                //if the jump fails :( take damage
+                actor.hurt(failDamage);
+                return actor + " failed to jump and took " + failDamage + " damage";
+            }
         }
-        return actor + "jumped " + direction;
     }
 
+    /**
+     *
+     * @param actor The actor performing the action.
+     * @return A description of the potential action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return "Jump to " + direction;
