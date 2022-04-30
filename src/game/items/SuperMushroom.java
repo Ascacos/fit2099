7c1c68503968362a09880414ad12a87b4be8d01d
apas0022 <apas0022@student.monash.edu>
2022-04-30 13:44:48 +1000

    Made SuperMushroom extend Comsumable (rather than Item) in order to implement the comsumable functionality

--- src/game/items/SuperMushroom.java
+++ src/game/items/SuperMushroom.java
@@ -1,16 +1,23 @@
 package game.items;
 
-import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.actors.Actor;
+import game.Status;
+import game.actions.ConsumeItemAction;
 
-public class SuperMushroom extends Item {
+public class SuperMushroom extends Consumable {
 
     /***
      * Constructor.
-     *  @param name the name of this Item
-     * @param displayChar the character to use to represent this item if it is on the ground
-     * @param portable true if and only if the Item can be picked up
      */
-    public SuperMushroom(String name, char displayChar, boolean portable) {
-        super(name, displayChar, portable);
+    public SuperMushroom() {
+        super("Super Mushroom", '^', true);
+        this.addAction(new ConsumeItemAction(this));
+    }
+
+    @Override
+    public void consume(Actor actor) {
+
+        actor.increaseMaxHp(50);
+        actor.addCapability(Status.SUPER_MUSHROOM);
     }
 }
