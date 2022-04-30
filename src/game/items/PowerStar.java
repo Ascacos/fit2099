eb39f086c64a41d9fc8fb5d89e3e816bbabef314
apas0022 <apas0022@student.monash.edu>
2022-04-30 18:51:21 +1000

    Fixed bug where the PowerStar's lifetime was not being decremented when in the player's inventory & added functionality to remove it from the game after 10 turns.

--- src/game/items/PowerStar.java
+++ src/game/items/PowerStar.java
@@ -2,9 +2,15 @@ package game.items;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
+import game.actions.ConsumeItemAction;
 
 public class PowerStar extends Consumable {
 
+    /**
+     * The amount to heal an Actor, when consumed.
+     */
+    private final int healAmount;
     /**
      * The lifetime of the Power Star.
      */
@@ -12,23 +18,39 @@ public class PowerStar extends Consumable {
 
     /***
      * Constructor.
-     * @param name the name of this Item
-     * @param displayChar the character to use to represent this item if it is on the ground
-     * @param portable true if and only if the Item can be picked up
      */
-    public PowerStar(String name, char displayChar, boolean portable) {
-        super(name, displayChar, portable);
-        lifetime = 10;
+    public PowerStar() {
+        super("Power Star", '*', true);
+        this.lifetime = 10;
+        this.healAmount = 200;
+
+        this.addAction(new ConsumeItemAction(this));
     }
 
     @Override
     public void tick(Location currentLocation) {
         System.out.println(lifetime);
         this.lifetime -= 1;
+
+        if (lifetime == 0) {
+            currentLocation.removeItem(this);
+        }
+    }
+
+    @Override
+    public void tick(Location currentLocation, Actor actor) {
+        System.out.println(lifetime);
+        this.lifetime -= 1;
+
+        if (lifetime == 0) {
+            actor.removeItemFromInventory(this);
+        }
     }
 
     @Override
     public void consume(Actor actor) {
 
+        actor.heal(healAmount);
+        actor.addCapability(Status.POWER_STAR);
     }
 }
