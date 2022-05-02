493bc82cc01e2093e34b5d1070e7b043499d4648
apas0022 <apas0022@student.monash.edu>
2022-05-02 17:16:44 +1000

    Added JavaDocs to AddMoneyAction.java

--- src/game/actions/AddMoneyAction.java
+++ src/game/actions/AddMoneyAction.java
@@ -6,14 +6,38 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import game.bank.WalletManager;
 import game.items.Coin;
 
+/**
+ * An Action class for adding money to an Actor, through a coin.
+ * @see edu.monash.fit2099.engine.actions.Action
+ */
 public class AddMoneyAction extends Action {
 
+    /**
+     * The coin attached to this action.
+     * @see game.items.Coin
+     */
     private final Coin coin;
 
+    /**
+     * Constructor
+     * @param coin The coin item attached to this Action
+     */
     public AddMoneyAction(Coin coin) {
         this.coin = coin;
     }
 
+    /**
+     * Executes this AddMoneyAction.
+     *
+     * This method will check if the Actor executing the action exists in the {@link game.reset.ResetManager ResetManager}
+     * and subsequently determine whether or not this Action will succeed. If the Actor does not have a
+     * {@link game.bank.Wallet Wallet}, this method will do nothing on execution. If the Actor does have a Wallet,
+     * it's balance will be incremented by the value of the {@link game.items.Coin Coin}.
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return A description of what happened on execution.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
 
@@ -31,6 +55,12 @@ public class AddMoneyAction extends Action {
         return menuDescription(actor);
     }
 
+    /**
+     * A method to return a descriptive string
+     * @see edu.monash.fit2099.engine.actions.Action#menuDescription(Actor)
+     * @param actor The actor performing the action.
+     * @return A string to display in the game menu.
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " gets $" + coin.getValue();
