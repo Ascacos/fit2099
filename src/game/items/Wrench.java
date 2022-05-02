994abf9244c68d01f3bf96c3361a13e3f915047d
apas0022 <apas0022@student.monash.edu>
2022-05-02 15:15:03 +1000

    Added a base Wrench item class

---
+++ src/game/items/Wrench.java
@@ -0,0 +1,14 @@
+package game.items;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class Wrench extends WeaponItem {
+    /**
+     * Constructor.
+     */
+    public Wrench() {
+        super("Wrench", 'w', 50, "slaps", 80);
+    }
+
+
+}
