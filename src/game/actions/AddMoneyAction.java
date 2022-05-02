c2e9364682d1dc2b9cb3021c344da720748942d8
apas0022 <apas0022@student.monash.edu>
2022-05-02 11:09:30 +1000

    Converted AddMoneyAction to use a Coin object rather than an integer value

--- src/game/actions/AddMoneyAction.java
+++ src/game/actions/AddMoneyAction.java
@@ -4,13 +4,14 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.actors.Player;
+import game.items.Coin;
 
 public class AddMoneyAction extends Action {
 
-    private final int amount;
+    private final Coin coin;
 
-    public AddMoneyAction(int amount) {
-        this.amount = amount;
+    public AddMoneyAction(Coin coin) {
+        this.coin = coin;
     }
 
     @Override
@@ -19,17 +20,15 @@ public class AddMoneyAction extends Action {
         //FIXME: we cannot use instanceof in our final submission (WILL LOSE MARKS)
         if (actor instanceof Player) {
 
-            ((Player) actor).addMoney(this.amount);
+            ((Player) actor).addMoney(coin.getValue());
 
-            //TODO: How to delete coin after being picked up?
-            // this line is removing *all* items, not just the coin that was picked up
-            map.at(map.locationOf(actor).x(), map.locationOf(actor).y()).getItems().clear();
+            map.locationOf(actor).removeItem(coin);
         }
         return menuDescription(actor);
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " gets $" + amount;
+        return actor + " gets $" + coin.getValue();
     }
 }
