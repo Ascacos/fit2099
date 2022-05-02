e5c43451bd3e89c4baf2383ad5d6abbccdcc9810
apas0022 <apas0022@student.monash.edu>
2022-05-02 16:23:01 +1000

    Completed execute() function in BuyItemAction to properly manage trading, through the WalletManager singleton.

--- src/game/actions/BuyItemAction.java
+++ src/game/actions/BuyItemAction.java
@@ -4,6 +4,8 @@ import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.bank.Wallet;
+import game.bank.WalletManager;
 
 public class BuyItemAction extends Action {
 
@@ -24,13 +26,25 @@ public class BuyItemAction extends Action {
 
     @Override
     public String execute(Actor actor, GameMap map) {
-        actor.addItemToInventory(item);
 
-        return actor + " bought " + item + " for " + cost;
+        WalletManager wallets = WalletManager.getInstance();
+        if (wallets.getWallets().containsKey(actor)) {
+
+            Wallet wallet = wallets.getWallets().get(actor);
+            if (wallet.getBalance() >= cost) {
+                wallet.takeMoney(cost);
+                actor.addItemToInventory(item);
+                return actor + " bought " + item;
+            } else {
+                return actor + " doesn't have enough coins!";
+            }
+        } else {
+            return actor + " doesn't have a wallet!";
+        }
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return actor + " buys " + item;
+        return actor + " buys " + item + "($" + cost + ")";
     }
 }
