ec497231fcf9b91a5e83915e8e7817b09da87a94
apas0022 <apas0022@student.monash.edu>
2022-05-02 15:14:14 +1000

    Attempted to add functionality for Koopa dormant state (not functional)

--- src/game/actors/enemies/Koopa.java
+++ src/game/actors/enemies/Koopa.java
@@ -8,6 +8,8 @@ import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
 import edu.monash.fit2099.engine.weapons.Weapon;
+import game.Status;
+import game.behaviours.DormantBehaviour;
 
 public class Koopa extends Enemy {
     /**
@@ -28,12 +30,21 @@ public class Koopa extends Enemy {
         //Check for Reset
         super.playTurn(actions, lastAction, map, display);
 
+        if (isDormant()) {
+            this.behaviours.put(1, new DormantBehaviour());
+        }
+
         for(game.behaviours.Behaviour Behaviour : behaviours.values()) {
             Action action = Behaviour.getAction(this, map);
             if (action != null)
                 return action;
         }
+
         return new DoNothingAction();
+    }
+
+    private boolean isDormant() {
+        return this.hasCapability(Status.DORMANT);
     }
 
     @Override
@@ -44,5 +55,25 @@ public class Koopa extends Enemy {
     @Override
     public IntrinsicWeapon getIntrinsicWeapon() {
         return new IntrinsicWeapon(30, "punches");
+    }
+
+    @Override
+    public boolean isConscious() {
+        return !super.isConscious() || isDormant();
+    }
+
+    @Override
+    public void hurt(int points) {
+        System.out.println("Koopa hp: " + printHp());
+        super.hurt(points);
+        if (!super.isConscious()) {
+            this.addCapability(Status.DORMANT);
+        }
+    }
+
+    @Override
+    public char getDisplayChar() {
+        if (this.isDormant()) return 'D';
+        else return super.getDisplayChar();
     }
 }
